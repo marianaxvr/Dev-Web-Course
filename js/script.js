d62289e05a8e913958a86d0a7119ece6a373addf
Mariana <mdxavier@live.com>
2023-12-08 15:53:18 -0300

    Introdução ao JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("Olá, mundo!");
+// var response = confirme("Deseja excluir?"); <- modelos de exercícios explicados pelo curso
+
+// console.log(response);
+
+var a = 10;
+var b = 33;
+// alert(a + b);
+
+function somar(a, b){
+    return a + b;
+}
+console.log(somar(2, 5));
