e3de45b34ba4a6d2c1032efdf328907999bc7fe8
Mariana <mdxavier@live.com>
2023-12-08 15:56:54 -0300

    Aula 10

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 
 // console.log(response);
 
-var a = 10;
-var b = 33;
+const a = 10;
+const b = 33;
 // alert(a + b);
 
 function somar(a, b){
     return a + b;
 }
-console.log(somar(2, 5));
+// console.log(somar(2, 5));
